4bbaa0df08c1c7fe6cb59dc0e917989522fa71ba
George Finch <georgefinch12@gmail.com>
2025-07-16 17:36:17 +0100

    fish catching.
    can now catch and relase a fish.
    fish now goes back to start when just relased.
    staggered fish relsease.

--- index.js
+++ index.js
@@ -5,43 +5,96 @@ const ctx = canvas.getContext("2d");
 const movementSpeed = 5;
 
 let mouseY = 0;
-let fish ={
- postionX:0,
- postionY:0,
- hit:false,
-}
+
 let line={
     caught:false,
+    fish:null,
 }
 const fishImg = new Image;
 fishImg.src="./assets/fluffy.png"
 const fishBiteImg = new Image;
 fishBiteImg.src="./assets/fluffy_bite.png"
 
+
+
+
+class fish{
+     constructor() {
+this.postionX=0,
+ //so doesn't start on land
+ this.postionY=150,
+ this.hit=false,
+ this.justReleased=false,
+ this.intialDraw = true
+ //how long delayed by
+ this.startDelay = Math.random()*1000
+    }
+}
+
+const fishes = [new fish, new fish, new fish, new fish]
+
+
+
+
+
+
+
+
+
 function setup(){
     canvas.width = window.innerWidth;
     canvas.height = window.innerHeight;
+   fishes.forEach(x => {
+    //so randomise the position
+    fishRestartPostion(x)
+   });
     draw();
+
 }
 function draw(){
      ctx.fillStyle = "rgb(255,255,255)"
     ctx.fillRect(0, 0, window.innerWidth, window.innerHeight)
-    catchDetect(fish)
+    fishes.forEach(x => {
+        //to stagger them at start 
+    if(x.intialDraw && x.startDelay <=0){
+x.intialDraw = false
+
+
+    }else{
+        x.startDelay = x.startDelay -5;
+    }
+    if(x.startDelay <= 0){
+    fishReturned(x)
+    catchDetect(x)
+    fishDraw(x)
+    }
+   });
+    
+    
     gameEnviorment();
-    fishDraw()
+    
     fishingLine();
     setTimeout(draw, 20)
 }
 
- function fishDraw(){
-    if(!fish.hit){
-    //so if hit side 
-    if(fish.postionX >=window.innerWidth){
-fish.postionX = 0;
-fish.postionY = Math.random()*window.innerHeight;
+function fishRestartPostion(x){
+x.postionX = 0;
+        
+x.postionY = Math.random()*((window.innerHeight-50)-150)+ 150;
+}
+ function fishDraw(x){
+    if (x.justReleased){
+        fishRestartPostion(x)
+x.justReleased = false
     }
-   fish.postionX = fish.postionX + movementSpeed;
-ctx.drawImage(fishImg,fish.postionX,fish.postionY,100,50);
+    if(!x.hit){
+    //so if hit side 
+    if(x.postionX >=window.innerWidth){
+fishRestartPostion(x)
+    }
+   x.postionX = x.postionX + movementSpeed;
+ctx.drawImage(fishImg,x.postionX,x.postionY,100,50);
+
  
  }
 
@@ -61,7 +114,7 @@ function fishingLine() {
 // so can undo the changes
 if(line.caught){
     //-35 so it looks like it eating the line.
-    
+
     ctx.drawImage(fishBiteImg,(window.innerWidth /2)-35,mouseY-10,50,100);
   
 }
@@ -71,10 +124,24 @@ function catchDetect(fishInput){
     //+100 on x so is head that hits
     //50 is fish hight so if line is between it
     // as 5 intervals need to allow for that
-if((fishInput.postionX+90 <= window.innerWidth /2 && fishInput.postionX+110 >= window.innerWidth /2) && (fishInput.postionY<mouseY && fishInput.postionY + 50 > mouseY) ){
+    //!line.caught = so only one fish at a time
+if(!line.caught&&((fishInput.postionX+90 <= window.innerWidth /2 && fishInput.postionX+110 >= window.innerWidth /2) && (fishInput.postionY<mouseY && fishInput.postionY + 50 > mouseY)) ){
 line.caught = true;
-fish.hit = true;
+fishInput.hit = true;
+line.fish = fishInput;
 
+}
+}
+//neeed fish so know which one to respawn in
+function fishReturned(x){
+//125 not 150 so fish a bit over the lne
+    if(line.fish === x && line.caught && mouseY<125){
+        line.caught = false
+        line.fish = null,
+        x.hit = false
+        x.justReleased = true
+        
+    
 }
 }
 function gameEnviorment(){
